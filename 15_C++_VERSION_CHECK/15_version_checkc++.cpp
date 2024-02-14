84498555b88582e6949deb65b1a21a767d54c905
AAKASH-YADAV-CODER <ay614838@gmail.com>
2024-02-14 11:05:04 +0530

    Version Check File📈

---
+++ 15_C++_VERSION_CHECK/15_version_checkc++.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    switch (__cplusplus)
+    {
+    case 202002L:
+        cout << "C++20" << endl;
+        break;
+    case 201703L:
+        cout << "C++17" << endl;
+        break;
+    case 201402L:
+        cout << "C++14" << endl;
+        break;
+    case 201103L:
+        cout << "C++11" << endl;
+        break;
+    case 199711L:
+        cout << "C++98" << endl;
+        break;
+    default:
+        cout << "pre-Standard C++" << endl;
+    }
+    return 0;
+}
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     if (__cplusplus == 202002L)
+//         std::cout << "C++20" << endl;
+//     else if (__cplusplus == 201703L)
+//         std::cout << "C++17" << endl;
+//     else if (__cplusplus == 201402L)
+//         std::cout << "C++14" << endl;
+//     else if (__cplusplus == 201103L)
+//         std::cout << "C++11" << endl;
+//     else if (__cplusplus == 199711L)
+//         std::cout << "C++98" << endl;
+//     else
+//         std::cout << "pre-Standard C++" << endl;
+//     return 0;
+// }
